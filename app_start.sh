6a63a448d95a8a63bcf0567d0f74136a9e4efa85
lp55323 <315843476@qq.com>
2023-04-11 23:27:28 +0800

    Add files via upload

--- app_start.sh
+++ app_start.sh
@@ -2,7 +2,7 @@
 sudo yum install -y httpd 
 sudo setenforce 0
 sudo chmod 777 -R /var/www/html/
-sudo ifconfig > /var/www/html/index.html
+sudo hostname > /var/www/html/index.html
 
 sudo systemctl start httpd
 sudo systemctl enable httpd
